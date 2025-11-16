0fbe19db45bbd487f52c374b5333494b9c52ca10
mian-adoan98 <mian.adoan98@outlook.com>
2025-11-16 20:48:06 +0100

    add new code & modified code in collector.py and ingestor.py; removed code implementation of DataSaver from collector.py and moved to saver.py

--- scripts/collection/collector.py
+++ scripts/collection/collector.py
@@ -44,51 +44,7 @@ class DataLoader(DataCollector):
         dataset = pd.read_csv(self.file, index_col=0)
         return dataset
     
-## Implement DataSaver class
-class DataSaver:
-    # Initialise DataSaver-attributes
-    def __init__(self, folder):
-        self.project_path = "C:\Development\Projects\MachineLearning\Laptop-Price-Predictor-System"
-        self.folder_path = os.path.join(self.project_path, folder)
-        
 
-    # Method 1: saving multiple datasets into a folder 
-    def save_multiple_ds(self, data_dict: dict):
-        """data_dict: 
-            - Key: filename assinging to the dataset
-            - Value: dataset that will be stored"""
-        # Check if folder exists
-        if not os.path.exists(self.folder_path):
-            os.makedirs(self.folder_path)
-
-        # Iteration: save every dataset into the selected path
-        file_idx = 0
-        for filename, dataset in data_dict.items():
-            # Filename as csv-files
-            csv_filename = f"{filename}.csv"
-
-            # Create a file path
-            self.file = os.path.join(self.folder_path, csv_filename) 
-            dataset.to_csv(self.file)
-            
-            # Display file saving process
-            file_idx += 1
-            print(f"File {file_idx}: dataset as {filename}.csv --> {self.folder_path} (Saving is successful")
-
-    # Method 2: saving one single dataset into a folder 
-    def save_one_ds(self, dataset: pd.DataFrame, filename: str):
-        # Check if folder exists
-        if not os.path.exists(self.folder_path):
-            os.makedirs(self.folder_path)
-
-        # Save dataset to selected folder 
-        # Filename as csv-files
-        csv_filename = f"{filename}.csv"
-
-        # Create a file path
-        self.file = os.path.join(self.folder_path, csv_filename) 
-        dataset.to_csv(self.file)
-        print(f"File {csv_filename} has been stored successfully")
 
 # Main space: Example code
 if __name__ == "__main__":
--- scripts/collection/ingestor.py
+++ scripts/collection/ingestor.py
@@ -14,33 +14,63 @@ class Ingestor(ABC):
         self.source_folder = os.path.join(self.path, folder)
         self.source_filename = filename
 
-    # Method 1: Load the data
+    # Method 1: Validate the data
     @abstractmethod
-    def load_data(self, format:str) -> pd.DataFrame:
+    def validate_data(self, extension: str):
         pass 
 
-    # Method 2: Validate the data
-    @abstractmethod
-    def validate_data(self):
-        pass 
-
-    # Method: Ingest data from selected folder 
+    # Method 2: Ingest data from selected folder 
     @abstractmethod
     def ingest(self):
         pass 
 
 # Implement FileIngestor
 class FileIngestor(Ingestor):
-    # Method 1: Load the data 
-    def load_data(self, format:str) -> pd.DataFrame:
-        # Check the format of dataset
-        if format == ".csv" and self.source_filename.endswith(format):
-            csv_file = os.path.join(self.source_folder, f"{self.source_filename}.{format}")
-            dataset = pd.read_csv(csv_file)
-        elif format == ".sql" and self.source_filename.endswith(format):
-            database = 
-        # load the dataset
+    # Intialise variables: ingesting daa
+    def __init__(self, folder, filename):
+        self.path = "C:\Development\Projects\MachineLearning\Laptop-Price-Predictor-System\data"
+        self.source_folder = os.path.join(self.path, folder)
+        self.source_filename = filename
 
+    # Method 1: ingest data
+    def ingest(self) -> pd.DataFrame:
+        # Extract format of the file
+        format = self.source_filename.split(".")[-1]
+
+        # Check which file is approved for ingestion
+        # Ingesting file with csv-extension
+        if self.source_filename.endswith(format):
+            csv_file = os.path.join(self.source_folder, self.source_filename)
+            data = pd.read_csv(csv_file, index_col=0)
+        
+        # Ingesting file with sql-extension
+        elif format == ".sql" and self.source_filename.endswith(format):
+            sql_name = os.path.join(self.source_folder,self.source_filename)
+            data = pd.read_sql(sql_name)
+     
+        # Ingesting file with html-extension
+        return data
+    
+   # Method 2: Validate data
+    def validate_data(self, extension: str):
+        # if filename exist in folder 
+        if self.source_filename in self.source_folder:
+            print(f"File {self.source_filename} exists.")
+        else:
+            print(f"Source file {self.source_filename} does not exist. Specify the filename correctly.")
+        
+        # check filename with correct format
+        extensions = [".csv", ".txt", ".sql"]
+        file_extension = self.source_filename.split(".")[-1]
+        if file_extension in extensions:
+            print(f"File extions is approved")
+        else:
+            print(f"File extension disapproved. Specify the file extension correctly")
+
+
+# Implement class ZipfileIngestor
+class ZipFileIngestor(Ingestor):
+    pass 
 
 
 # Examples code 
